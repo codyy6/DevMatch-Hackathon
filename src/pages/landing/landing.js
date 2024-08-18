c6c4ed01581246801e39ea5f0ce93d4f0bf7d282
Cody <codyyliew@gmail.com>
2024-08-18 10:46:16 +0800

    simple navbar

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios'; // Import axios for making HTTP requests
 import { AnyPublicKey } from '@aptos-labs/ts-sdk';
-
+import { Header } from 'antd/es/layout/layout';
+import Navbar from '../../components/Header/Header';
 const API_URL = "https://service-testnet.maschain.com";
 const client_id = "2f865b0086add601eb6788d6a5e50d68a8abf8cf9607c9239669f5b1a529d14c";
 const client_secret = "sk_f7f377c5c2e1d3d795265fbde212f0276bd0509aba86178236c34ae7f9dcda08";
@@ -45,9 +46,26 @@ async function createSmartContract() {
     }
 }
 
-async function getCertificate() {
-
-
+async function getCertificate(from, contractAddress, status) {
+  const to = prompt('Enter the recipient wallet address:'); 
+  try {
+    const response = await axios.get(`${API_URL}/api/certificate/get-certificate`, {
+      params: {
+        from,
+        to,
+        contractAddress,
+        status
+      },
+      headers: {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "content-type": "application/json"
+      }
+    });
+    console.log('Certificate details:', response.data.result);
+  } catch (error) {
+    console.error('Error fetching certificate details:', error.response ? error.response.data : error.message);
+  }
 }
 
 
@@ -147,9 +165,11 @@ function Landing() {
   };
 
   return (
+    <div>
+    <Navbar/>
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <button onClick={handleCreateClick}>Create Smart Contract</button>
-      <button onClick={handleFetchClick}>Fetch Smart Contracts</button>
+      {/* <button onClick={handleCreateClick}>Create Smart Contract</button>
+      <button onClick={handleFetchClick}>Fetch Smart Contracts</button> */}
       {error && <p>Error: {error.message}</p>}
       <ul>
         {/* {smartContracts.map(contract => (
@@ -160,8 +180,9 @@ function Landing() {
            </li>
         ))}  */}
         <button onClick={() => handleMintClick()}>Mint Certificate</button>
-        <button onClick={() => getCertificate()}>Check Certificate</button>
+        <button onClick={() => getCertificate(from, contract_address, status)}>Check Certificate</button>
       </ul>
+    </div>
     </div>
   );
 }
